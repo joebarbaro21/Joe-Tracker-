975a625fe2ef73ae83c5aaf440fb9a5a5d2530f0
joebarbaro21 <ps7nww5grg@privaterelay.appleid.com>
2026-05-26 14:23:01 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        minHeight: "100vh",
+        padding: 40,
+        fontSize: 32,
+      }}
+    >
+      SITE WORKS
+    </div>
+  );
+}
